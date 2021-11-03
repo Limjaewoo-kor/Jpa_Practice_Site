82fcda796c3356cf3b0a9101aee7e9d6962e16db
Limjaewoo <ljw3944@naver.com>
2021-11-03 21:44:47 +0900

    Mmmo OAuth2_naver

--- src/main/java/com/Mmmo/web/indexController.java
+++ src/main/java/com/Mmmo/web/indexController.java
@@ -1,5 +1,6 @@
 package com.Mmmo.web;
 
+import com.Mmmo.config.auth.LoginUser;
 import com.Mmmo.config.auth.dto.SessionUser;
 import com.Mmmo.service.posts.PostsService;
 import com.Mmmo.web.dto.PostsResponseDto;
@@ -16,12 +17,12 @@ import javax.servlet.http.HttpSession;
 public class indexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+       // SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
